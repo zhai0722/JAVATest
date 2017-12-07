9f138c79109107e32ff5ac5e02137785bd95fc62
zhai0722 <zhai0722@163.com>
2017-12-07 22:29:42 +0800

    Initial commit

---
+++ src/ExtendDemo1.java
@@ -0,0 +1,38 @@
+/**
+ * Created by Think on 2017/12/5.
+ */
+public class ExtendDemo1 {
+    public static void main(String[] args) {
+BMWCar car1=new BMWCar();
+        System.out.println(car1.speed);
+    }
+}
+class Car {
+    public String color;
+
+    public void run()
+    {
+        System.out.println("running");
+    }
+}
+class SportCar extends Car{
+
+    public int speed;
+    public  SportCar(int speed)
+    {
+        this.speed=speed;
+        System.out.println("new sport car");
+
+    }
+}
+class BMWCar extends SportCar
+{
+    public int price;
+
+
+
+    public BMWCar() {
+        super(220);
+        System.out.println("new BMWSportCar");
+    }
+}

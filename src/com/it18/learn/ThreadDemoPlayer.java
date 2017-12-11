e0c5419614b8e5b8d8c6461a277351845ee09d3e
zhai0722 <zhai0722@163.com>
2017-12-11 23:50:32 +0800

    thread

---
+++ src/com/it18/learn/ThreadDemoPlayer.java
@@ -0,0 +1,47 @@
+package com.it18.learn;
+
+/**
+ * Created by Think on 2017/12/11.
+ */
+public class ThreadDemoPlayer {
+    public static void main(String[] args) throws InterruptedException {
+        player p1= new player("1",5000);
+        player p2= new player("2",1000);
+        player p3= new player("3",8000);
+        player p4= new player("4",3000);
+        p1.start();
+        p2.start();
+        p3.start();
+        p4.start();
+        //start 放一起 join放一起才能多线程
+        p1.join();//等到P1线程执行完后继续执行
+        p2.join();//等到P2线程执行完后继续执行
+        p3.join();//等到P3线程执行完后继续执行
+        p4.join();//等到P4线程执行完后继续执行
+        System.out.println("人齐了开局" );
+    }
+}
+
+class player extends  Thread
+{
+    private  String name;
+    private  int time;
+    public  player (String name,int time)
+    {
+        this.name=name;
+        this.time=time;
+    }
+
+    public  void run()
+    {
+        System.out.println("玩家"+name+"出发");
+        try {
+            //静态方法让当前线程休眠time毫秒
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("玩家"+name+"到了");
+
+    }
+}
---
+++ src/com/it18/learn/ThreadDemoticket3.java
@@ -0,0 +1,52 @@
+package com.it18.learn;
+
+/**
+ * Created by Think on 2017/12/11.
+ */
+public class ThreadDemoticket3 {
+    public static void main(String[] args) {
+        ticketpool pool=new ticketpool();
+        salers s1=new salers("zhang",pool);
+        salers s2=new salers("wang",pool);
+        salers s3=new salers("dere",pool);
+        salers s4=new salers("ccd",pool);
+        s1.start();
+        s2.start();
+        s3.start();
+        s4.start();
+    }
+}
+class  ticketpool{//票池
+    private int tickets=100;
+    public  synchronized int gettick(){//同步方法块
+        int ticket=tickets;
+        tickets--;
+        return  ticket;
+    }
+
+}
+
+class salers extends  Thread{ //售票员
+private  ticketpool pool =null;
+private String name;
+public salers(String name,ticketpool pool)
+{
+    this.name=name;
+    this.pool=pool;
+
+}
+public  void run(){
+    while (true)
+    {
+        int no=pool.gettick();
+        if(no>0)
+        {
+            System.out.println(name+":"+no);
+        }
+        else {
+            return;
+        }
+    }
+}
+
+}

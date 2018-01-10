c2e201f830d2dce4b4351312417e936d378ce87c
zhai0722 <zhai0722@163.com>
2018-01-10 23:27:57 +0800

    add

--- SpringDemo/src/main/java/commanddemo1/CLI.java
+++ SpringDemo/src/main/java/commanddemo1/CLI.java
@@ -10,10 +10,11 @@ public class CLI {
         String arg[]={"-t 1000"};
         testParser(arg);
 
-      String argb[]={"-h"};
-      testParser(argb);
-
-    }
+//      String argb[]={"-h"};
+//      testParser(argb);
+      String argc[]={"-block-size=10"};
+      testParser(argc);
+  }
 
 
     public  void testParser(String[] args) throws ParseException {
@@ -21,13 +22,16 @@ public class CLI {
         Options options = new Options();
         options.addOption("h",false,"list help");//false代表不强制有
         options.addOption("t",true,"set time on system");
-
+        options.addOption( OptionBuilder.withLongOpt( "block-size" )
+                .withDescription( "use SIZE-byte blocks" )
+                .hasArg()
+                .withArgName("SIZE")
+                .create() );
         //解析
         //1.3.1中已经弃用针对不同格式入参对应的解析器
         //CommandLineParser parser = new PosixParser();
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = parser.parse(options,args);
-        System.out.println(Integer.parseInt(cmd.getOptionValue("h")));
         //查询交互
         //你的程序应当写在这里，从这里启动
         if (cmd.hasOption("h")){
@@ -40,6 +44,10 @@ public class CLI {
         if (cmd.hasOption("t")){
             System.out.printf("system time has setted  %s \n",cmd.getOptionValue("t"));
             return;
+        }
+        if( cmd.hasOption( "block-size" ) ) {
+            // print the value of block-size
+            System.out.println( cmd.getOptionValue( "block-size" ) );
         }
 
         System.out.println("error");
